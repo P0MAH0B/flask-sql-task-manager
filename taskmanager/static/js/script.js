58efbdbbd3348a6590555b034abe9cd616109eb1
Ivan POMAHOB <3gopobo@gmail.com>
2025-02-18 17:55:42 +0000

    Add new option - Add Task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
+
+ // select initialization
+ let selects = document.querySelectorAll("select");
+ M.FormSelect.init(selects);
+
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
